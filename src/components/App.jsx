d50a5d0a93a831b39a0b6b08ba3d6fb2d7cc946b
domanskayelyzaveta <domanskaya_@ukr.net>
2023-10-02 17:30:55 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
 import { Component } from 'react';
+// import Loader from 'react-loader-spinner'; // Импортируем компонент спиннера
+// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'; // Подключаем стили спиннера
 
 export class App extends Component {}
+
+// const key = '39407568-0a070d3ffff032a43909d6f38';
+/// HTTP Request запит
+/// https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
